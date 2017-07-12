2a5541b78ef15c62ac54da3fdee0971b85b1fb24
zhaosi2017 <zhaosi2017@gmail.com>
2017-07-12 16:25:09 +0700

    ignore up

---
+++ config/db_back.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=43.229.38.236;dbname=oadb',
+    'username' => 'root',
+//    'password' => 'OASystem@Yii2+H+',
+    'password' => 'Stefen@1234',
+    'charset' => 'utf8',
+];
